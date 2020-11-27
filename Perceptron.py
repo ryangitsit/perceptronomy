eefb2cf1b46d7e94941dbccf4a6e16d4a9c974a9
wehzie <39304339+Wehzie@users.noreply.github.com>
2020-11-27 13:04:51 +0100

    Finished the main part (a,b,c,d)

--- Perceptron.py
+++ Perceptron.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 This is a preliminary version of a Perceptron Learning Algorithm.
@@ -8,52 +9,56 @@ This is a preliminary version of a Perceptron Learning Algorithm.
  - This was just an intuitive first draft to play around with.  I believe my indexing for w-updates is probably not corrent and that not all values are updated.
  - The sign equation is probably not correct.  This was just an intuitive guess.  I will look more carefully at it later.
  - Part D from assignment is not included.  However, the loop to create a new data set and run the experiment multiple times is present.  
- - Average accuracy is printed.  It becomes clear that for differnt P and N ratios, there are different accuracies, so part D makes sense.
+ - Average accuracy is printed.  It becomes clear that for different P and N ratios, there are different accuracies, so part D makes sense.
  - Have not looked at the bonus questions.
 
 """
 
-A = 0       # initialize average accuracy counter
-runs = 10   # How many experiments to run
+# perform a run on a single set of data
+def run_rosenblatt(N, P, n_max):
+    # a) generate data
+    X = np.random.normal(0, 1, (N, P))          # randomly generated feature vector matrix
+    Y = np.random.choice([1, -1], size=P)       # randomly generated plus/minus 1 labels
+    W = np.zeros((N, P))                # initialize weights to zero
 
-for z in range(runs):
+    # b) sequential training
+    for n in range(n_max):              # epoch loop        
+        E_list = []
+        for p in range(P):              # feature vector loop
 
-    P = 50                              # number of feature vectors 
-    N = 500                             # number of features 
-    X = np.random.normal(0,1,(N,P))     # randomly generated feature vector matrix
-    Y = np.random.randint(2, size=P)    # randomly generated binary labels 
-    n_max = 100                         # how many sweeps through data
+            # c) Rosenblatt algorithm
+            E = np.dot(np.transpose(W[:, p]), X[:, p]) * Y[p]       # dot weights with features and multiply with label sign (1-D real number)
+            E_list.append(E)
+            if E <= 0:
+                W[:, p] = W[:, p] + (1/N) * X[:, p] * Y[p]          # check if local potential is less than zero and update weight if necessary
 
-    for i in range(len(Y)):             # converting binary to plus/minus 1
-        if Y[i] == 0:
-            Y[i] = -1
-        
-    W = np.zeros((N,P))                 # initialize weights to zero
+        if all([e > 0 for e in E_list]): break                      # end training if all E > 0
 
-    for n in range(n_max):              # sweeping loop
-
-        for p in range(P-1):                # feature vector loop
-
-            E = np.dot(np.transpose(W[:,p]),X[:,p]) * Y[p]      # dotting weights with features and multiplying with label sign (1-D real number)
-
-            if E <= 0:                                          # check if local potential is less than zero
-                W[:,p+1] =  W[:,p] + (1/N)*X[:,p]*Y[p]          # update next weight if necessary
-
-
-    sign = np.sign((np.dot(np.transpose(W),X)).sum(axis=0))     # sign of sum of dotted columns (probably should fix this!)
-
-    correct = 0                     # check accuracy against labels
+    # check accuracy against labels
+    sign = np.sign((np.dot(np.transpose(W), X)).sum(axis=0))     # sign of sum of dotted columns
+    correct = 0
     for i in range(len(Y)):
-        if Y[i] == sign[i]:
-            correct += 1
-        accuracy = correct/len(Y)
+        if Y[i] == sign[i]: correct += 1
+    accuracy = correct/len(Y)
 
-    print(accuracy)
+    return accuracy
 
-    A += accuracy
+def main():
+    N = 20           # number of features
+    alpha = np.arange(0.75, 3.25, 0.25).tolist()
+    mean_acc = 0    # initialize average accuracy counter
+    n_D = 50        # number of experiments to run
+    n_max = 100     # max number of epochs (sweeps through data)
 
-A = A/runs # average accuracy across all runs 
+    # d) train on multiple randomized data sets
+    collect_acc = []
+    for run in alpha:                       # change parameters
+        P = int(run*N)                         
+        for rep in range(n_D):              # given the parameters average over n_D runs
+            accuracy = run_rosenblatt(N, P, n_max)
+            collect_acc.append(accuracy)
 
-print("AVG ACC = ", A)  # depending on values of N,P, and n-max, usually achieves .75-.9
+        mean_acc = np.mean(collect_acc)     # average accuracy across all runs
+        print(f"AVG ACC for P={P} is {mean_acc}")
 
-
+main()
